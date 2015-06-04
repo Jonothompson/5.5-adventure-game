72d79bec8ff8b7e4adf3ee8304ac7fe6e4079ba6
Hannah Jordan <hjorda0126@aol.com>
2015-06-04 17:10:28 -0400

    Four characters picked. Vent setup to lead from home to fight screen.

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -1,19 +1,16 @@
 this["JST"] = this["JST"] || {};
 this["JST"]["application"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     return "<h1>Hello</h1>\n";
-<<<<<<< HEAD
 },"useData":true});
 this["JST"]["fight"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "";
+    return "<h1> Hello! </h1>\n";
 },"useData":true});
 this["JST"]["home"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "";
+    return "<form class=\"heroes\">\n\n<div class=\"jake\">\n<input type=\"submit\" name=\"name\" value=\"submit\">\n</div>\n\n\n\n<div class=\"finn\">\n  <input type=\"submit\" name=\"name\" value=\"submit\">\n</div>\n\n\n\n<div class=\"fiona\">\n  <input type=\"submit\" name=\"name\" value=\"submit\">\n</div>\n\n\n\n<div class=\"cake\">\n  <input type=\"submit\" name=\"name\" value=\"submit\">\n</div>\n\n</form>\n";
 },"useData":true});
 this["JST"]["lose"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     return "";
 },"useData":true});
 this["JST"]["win"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     return "";
-=======
->>>>>>> 760a49a728978d4689fae180b62cd15bb84a2961
 },"useData":true});
