f6943acf99572efdbfdeaaf79df011b07a83f721
Megan Dunbar <meganleedunbar@gmail.com>
2015-06-04 17:21:09 -0400

    idk

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -1,6 +1,6 @@
 this["JST"] = this["JST"] || {};
 this["JST"]["application"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "<h1>Hello</h1>\n";
+    return "<h1>AdWINture Time</h1>\n";
 },"useData":true});
 this["JST"]["fight"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     return "<h1> Hello! </h1>\n";
