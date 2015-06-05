dd79181332d27080d884823771ff72cb6d409709
Megan Dunbar <meganleedunbar@gmail.com>
2015-06-05 12:03:57 -0400

    JS template updated, just not allowing module router to be defined

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,75 @@
-window.AdventureFight = AdventureFight || {};
-(function(){
+window.AdventureFight = window.AdventureFight || {};
+(function(module){
+  'use strict';
 
   // Declare a variable that will later store the selected character and enemy
   var selectedHero;
   var selectedVillain;
 
   // Define a constructor
+    $(document).ready(function(hero){
+    $('.home').html(JST['home']());
+    module.router = new module.AdventureRouter();
+    Backbone.history.start();
+});
+
+  AdventureFight.vent = _.extend({}, Backbone.Events);
+  AdventureFight.vent.on('choose:heroes', function(heroes) {
+   $('.heroes').html(JST['home'](heroes));
+  });
+
   AdventureFight.Hero = function(params) {
     _.extend(this, params);
   };
 
-  AdventureFight.vent = _.extend({}, Backbone.Events);
-  AdventureFight.vent.on('choose:heroes', function(characters) {
-   $('.characters').html(JST['home'](characters));
-  });
-
-  // Give all characters an attack function
+    // Give all characters an attack function
   AdventureFight.Hero.prototype.attack = function(villain) {
     villain.health = villain.health - this.attack;
-    Game.vent.trigger('health:changed');
+    AdventureFight.vent.trigger('health:changed');
   };
 
   // Create some character instances
   AdventureFight.heroes = {
     'Jake': new AdventureFight.Hero({
-      health: 12,
-      attack: 1
+      health: 100,
+      attack: 10
     }),
 
     'Finn': new AdventureFight.Hero({
-      health: 6,
-      attack: 2
+      health: 100,
+      attack: 10
+    }),
+
+    'Fiona': new AdventureFight.Hero({
+      health: 100,
+      attack: 10
+    }),
+
+    'Cake': new AdventureFight.Hero({
+      health: 100,
+      attack: 10
+    })
+  };
+
+  AdventureFight.Villain = function(params) {
+    _.extend(this, params);
+  };
+
+  // Give all villains an attack function
+  AdventureFight.Villain.prototype.attack = function(hero) {
+    hero.health = hero.health - this.attack;
+    AdventureFight.vent.trigger('health:changed');
+  };
+
+  AdventureFight.villains = {
+    'Ice King': new AdventureFight.Villain({
+      health: 100,
+      attack: 10
+    }),
+
+    'Magic Man': new AdventureFight.Villain({
+      health: 100,
+      attack: 10
     })
   };
 
@@ -39,7 +78,8 @@ window.AdventureFight = AdventureFight || {};
   });
 
   AdventureFight.vent.on('hero:submit', function(villain) {
-    selectedVillain = villain;
+    selectedVillain = _.sample(AdventureFight.Villain, [n]);
+    console.log(selectedVillain);
   });
 
   AdventureFight.vent.on('attack:villain', function() {
@@ -47,3 +87,4 @@ window.AdventureFight = AdventureFight || {};
   });
 
 })();
+(window.AdventureFight);
