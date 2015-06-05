5cf1bdb4635a52d4da530db36c6c43b48ed18621
Jonathan Thompson <JonathanLeighThompson@gmail.com>
2015-06-05 09:37:29 -0400

    merging in changes to main.js and templates for attack page with a basic non-functioning heroAttack.

--- scripts/main.js
+++ scripts/main.js
@@ -1,73 +1,99 @@
 window.AdventureFight = window.AdventureFight || {};
 
 (function(module){
-  'use strict';
+ 'use strict';
 
-var hero = selectedHero;
-var villain = selectedVillain;
+var hero = 'selectedHero';
+var villain = 'selectedVillain';
 
-  AdventureFight.vent = _.extend({}, Backbone.Events);
- AdventureFight.vent.on('choose:heroes', function(characters) {
-   $('.characters').html(JST['home'](characters));
- });
-
-
-  $(document).ready(function(hero){
-    $('.home').html(JST['home']());
-    module.router = new module.AdventureRouter();
-    Backbone.history.start();
-
-    function hero(selectedHero){
-      this.attack:
-      this.health:
-
-    }
-//Hannah prototype
-    AdventureFight.hero = {
-
-      'Jake' : new hero({
-        health: 100,
-        attack: 10
-      }),
-
-      'Finn' : new hero({
-        health: 100,
-        attack: 10
-      }),
-
-      'Fiona' : new hero({
-        health: 100,
-        attack: 10
-      }),
-
-      'Cake' : new hero({
-        health: 100,
-        attack: 10
-      }),
-
-    }
-
-    $(document).on('submit', '.heroes', function(event){
-   event.preventDefault();
-   AdventureFight.router.navigate('fight', {trigger: true});
- });
-
-function villain(){
-  this.attack:
-  this.health:
-  //create and assign randoms Meg
-}
-
-  var iceking = new villain('Ice King');
-  var lemongrab = new villain('Earl of Lemongrab')
-
-  $(document).on('submit', '.heroes', function(selectedVillain){
-    villain.random()
+AdventureFight.vent = _.extend({}, Backbone.Events);
+AdventureFight.vent.on('choose:heroes', function(characters) {
+  $('.characters').html(JST['home'](characters));
 });
 
-  });
+
+ $(document).ready(function(hero){
+   $('.home').html(JST['home']());
+   module.router = new module.AdventureRouter();
+   Backbone.history.start();
+
+    function hero(selectedHero){
+      this.attack
+      this.health
+    }
+//Hannah prototype
+   AdventureFight.hero = {
+
+     'Jake' : new hero({
+       health: 100,
+       attack: 10
+     }),
+
+     'Finn' : new hero({
+       health: 100,
+       attack: 10
+     }),
+
+     'Fiona' : new hero({
+       health: 100,
+       attack: 10
+     }),
+
+     'Cake' : new hero({
+       health: 100,
+       attack: 10
+     }),
+
+   }
+   
+   function heroAttack(hero, villain){
+        villain.health - hero.attack;
+        return villian.health;
+        console.log(villian.health);
+   }
+   
+/////// villain section ////////
+
+   function villain(selectedVillain){
+     this.attack;
+     this.health;
+   }
+
+   AdventureFight.villain = {
+
+     'Ice King' : new villain({
+       health: 100,
+       attack: 10
+     }),
+
+     'Earl of Lemongrab' : new villain({
+       health: 100,
+       attack: 10
+     }),
+
+     'Princess Bubblegum' : new villain({
+       health: 100,
+       attack: 10
+     }),
+
+     'Magic Man' : new villain({
+       health: 100,
+       attack: 10
+     }),
+
+   }
+
+ $(document).on('submit', '.heroes', function(event){
+     event.preventDefault();
+     AdventureFight.router.navigate('fight', {trigger: true});
+     villain = villain[Math.floor(Math.random())];
+     console.log(villain);
+});
+
+ $(document).on('submit', '.hero', function(event){
+     event.preventDefault();
+     heroAttack();
+});
+
+ });
 })(window.AdventureFight);
-
-
-
-})();
