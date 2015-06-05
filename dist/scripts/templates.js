2bfd9a137c194b9d4caf10e100ec6845dd824136
Jonathan Thompson <JonathanLeighThompson@gmail.com>
2015-06-05 11:26:47 -0400

    this commit dosent matter.

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -3,7 +3,7 @@ this["JST"]["application"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1
     return "<h1>AdWINture Time</h1>\n";
 },"useData":true});
 this["JST"]["fight"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "<div class=\"hero\">\n	<input type=\"submit\" name=\"name\" value=\"submit\">\n</div>\n\n<div class=\"villian\">\n	<input type=\"submit\" name=\"name\" value=\"submit\">\n</div>\n";
+    return "<div class=\"hero\">\n	<input type=\"submit\" name=\"name\" value=\"fight\">\n</div>\n\n<div class=\"villian\">\n	<input type=\"submit\" name=\"name\" value=\"submit\">\n</div>\n";
 },"useData":true});
 this["JST"]["home"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     return "\n<div class=\"homePage\">\n\n\n\n<section class=\"logo\">\n  <img src=\"../logo.png\">\n</section>\n\n<div class=\"choose\">\n  <p>Choose your Adventurer!</p>\n</div>\n\n<form class=\"heroes\">\n\n\n<div class=\"finn\">\n  <input type=\"image\" name=\"name\" src=\"../finn.png\" height=\"230\">\n</div>\n\n\n<div class=\"jake\">\n  <input type=\"image\" name=\"name\" src=\"../jake.png\" height=\"230\" width=\"150\">\n</div>\n\n\n<div class=\"fionna\">\n  <input type=\"image\" name=\"name\" src=\"../fionna_8037.png\" height=\"230\" width=\"180\">\n</div>\n\n\n\n<div class=\"cake\">\n  <input type=\"image\" name=\"name\" src=\"../cake.png\" height=\"230\" width=\"230\">\n</div>\n\n</form>\n\n</div>\n";
